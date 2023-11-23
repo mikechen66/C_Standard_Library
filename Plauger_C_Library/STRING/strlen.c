1667ce96ef142c77521cc0c19ec8b2e32609ff50
mikechen66 <875661606@qq.com>
2023-11-23 15:35:33 +0800

    Add files via upload

---
+++ Plauger_C_Library/STRING/strlen.c
@@ -0,0 +1,13 @@
+/* strlen function */
+
+#include <string.h>
+
+size_t (strlen)(const char *s) {    
+    // find length of s[]
+    const char *sc;
+
+    for (sc = s; *sc != '\0'; ++sc) {
+        ;
+    }
+    return (sc - s);
+}
