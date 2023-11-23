56c8a8934db2f31d802d965d5754ed48a12c3d41
mikechen66 <875661606@qq.com>
2023-11-23 13:45:40 +0800

    Add files via upload

---
+++ Plauger_C_Library/MATH/asin.c
@@ -0,0 +1,6 @@
+/* asin function */
+#include <math.h>
+
+double (asin)(double x) {  // compute asin(x) 
+    return (_Asin(x, 0));
+}
---
+++ Plauger_C_Library/MATH/ceil.c
@@ -0,0 +1,10 @@
+/* ceil function */
+
+#include "xmath.h"
+
+double (ceil)(double x) { // compute ceil(x) 
+
+    double y = x;
+
+    return (_Dint(&y, 0) < 0 && 0.0 < x ? y + 1.0 : y);
+}
