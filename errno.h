766e0e02260cd0f513391835e7e5a0ddd9594e57
mikechen66 <875661606@qq.com>
2023-11-24 13:14:31 +0800

    Add files via upload

--- errno.h
+++ errno.h
@@ -1,45 +1,19 @@
-#ifndef _ERRNO_H
-#include <stdlib/errno.h>
-#if !defined _ISOMAC && !defined __ASSEMBLER__
+/* errno.h standard header */
 
-# if IS_IN (rtld)
-#  include <dl-sysdep.h>
-#  ifndef RTLD_PRIVATE_ERRNO
-#   error "dl-sysdep.h must define RTLD_PRIVATE_ERRNO!"
-#  endif
-# else
-#  define RTLD_PRIVATE_ERRNO	0
-# endif
+#ifndef _ERRNO
+#define _ERRNO
+#ifndef _YVALS
+#include <yvals.h>
+#endif
 
-# if RTLD_PRIVATE_ERRNO
-/* The dynamic linker uses its own private errno variable.
-   All access to errno inside the dynamic linker is serialized,
-   so a single (hidden) global variable is all it needs.  */
+// error codes 
+#define EDOM _EDOM
+#define ERANGE _ERANGE
+#define EFPOS _EFPOS
 
-#  undef  errno
-#  define errno rtld_errno
-extern int rtld_errno attribute_hidden;
+// ADD YOURS HERE
+#define _NERR _ERRMAX  // one more than last code
 
-# elif IS_IN_LIB && !IS_IN (rtld)
-
-#  undef  errno
-#  if IS_IN (libc)
-#   define errno __libc_errno
-#  else
-#   define errno errno		/* For #ifndef errno tests.  */
-#  endif
-extern __thread int errno attribute_tls_model_ie;
-
-# endif	/* IS_IN_LIB */
-
-# define __set_errno(val) (errno = (val))
-
-extern int *__errno_location (void) __THROW __attribute_const__
-#  if RTLD_PRIVATE_ERRNO
-     attribute_hidden
-#  endif
-;
-libc_hidden_proto (__errno_location)
-
-#endif /* !_ISOMAC && !__ASSEMBLER__ */
-#endif /* !_ERRNO_H */
+// declatations 
+extern int errno;
+#endif
