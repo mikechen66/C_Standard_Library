eb0bd9c51531aaa2d05e66a6750522edc76bf322
mikechen66 <875661606@qq.com>
2023-11-27 00:29:28 +0800

    Add files via upload

---
+++ Plauger_C_Library/STDIO/errno.h
@@ -0,0 +1,19 @@
+/* errno.h standard header */
+
+#ifndef _ERRNO
+#define _ERRNO
+#ifndef _YVALS
+#include <yvals.h>
+#endif
+
+// error codes 
+#define EDOM _EDOM
+#define ERANGE _ERANGE
+#define EFPOS _EFPOS
+
+// ADD YOURS HERE
+#define _NERR _ERRMAX  // one more than last code
+
+// declatations 
+extern int errno;
+#endif
