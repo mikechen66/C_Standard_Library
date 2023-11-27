b7b3fbb3c20712eef3e4bfc06d2e4d2cb3a4186e
mikechen66 <875661606@qq.com>
2023-11-28 06:40:55 +0800

    Add files via upload

--- Plauger_C_Library/_TEST/tctype.c
+++ Plauger_C_Library/_TEST/tctype.c
@@ -5,8 +5,9 @@
 #include <limits.h>
 #include <stdio.h>
 
-static void prclass(const char *name, int(*fn)(int)) {  // display a printable character class
+static void prclass(const char *name, int(*fn)(int)) {  
 
+    // display a printable character class
     int c;
 
     fputs(name, stdout);
@@ -25,12 +26,13 @@ int main() {   // test both macros and functions
     int c;
 
     // display printable classes
-    prclass("ispunct", Cispunct);
-    prclass("isdigit", Cisdigit);
-    prclass("islower", Cislower);
-    prclass("isupper", Cisupper);
-    prclass("isalpha", Cisalpha);
-    prclass("isalnum", Cisalnum);
+    prclass("ispunct", &ispunct);
+    prclass("isdigit", &isdigit);
+    prclass("isxdigit", &isxdigit);   
+    prclass("islower", &islower);
+    prclass("isupper", &isupper);
+    prclass("isalpha", &isalpha);
+    prclass("isalnum", &isalnum);
 
     // test macros for required characters
     for (s = "0123456789"; *s; ++s) {
@@ -48,15 +50,16 @@ int main() {   // test both macros and functions
     for (s = "!\"#%&'();<=>?[\\]*+,-./:^_{|}~"; *s; ++s) {
         assert(ispunct(*s));
     }
+    }
     for (s = "\f\n\r\t\v"; *s; ++s) {
-        assert(isspace(*s));
+        assert(isspace(*s) && iscntrl(*s));
     }
 
-    assert(isspace('') && isprint(''));
+    assert(isspace(' ') && isprint(' '));
     assert(iscntrl('\a') && iscntrl('\b'));
 
     // test macros for all valid codes
-    for (c = EOF; c <= UCHAR_MAX; ++c) {   // test foir proper membership
+    for (c = EOF; c <= UCHAR_MAX; ++c) {   // test for proper class membership
         if (isdigit(c)) {
             assert(isalnum(c));
         }
@@ -79,12 +82,14 @@ int main() {   // test both macros and functions
             assert(isprint(c));
         }
         if (isspace(c)) {
-            assert(c == '' || !isprint(c))
+            assert(c == ' ' || !isprint(c));
         }
         if (iscntrl(c)) {
             assert(!isalnum(c));
         }
     }
+
+    // test functions for required characters
     for (s = "0123456789"; *s; ++s) {
         assert((isgidit)(*s) && (isxdigit)(*s));
     }
@@ -104,7 +109,7 @@ int main() {   // test both macros and functions
         assert((isspace)(*s) && (iscntrl)(*s));
     }
 
-    assert((isspace)('') && (isprint()(''));
+    assert((isspace)(' ') && (isprint()(' '));
     assert((iscntrl)('\a') && (iscntrl)('\b'));
     
     // test functions for all valid codes
@@ -131,7 +136,7 @@ int main() {   // test both macros and functions
             assert((isprint)(c));
         }
         if ((isspace)(c)) {
-            assert(c == '' || !(isprint)(c))
+            assert(c == ' ' || !(isprint)(c))
         }
         if ((iscntrl)(c)) {
             assert(!isalnum(c));
