7ee777cb5bda1bdef9da71ba9e18f3a37845be64
mikechen66 <875661606@qq.com>
2023-11-28 17:03:29 +0800

    Add files via upload

--- Plauger_C_Library/_HEADERS/xmath.h
+++ Plauger_C_Library/_HEADERS/xmath.h
@@ -15,7 +15,7 @@
 #define _DSIGN  0x8000
 #define DSIGN(x)    (((unsigned short *)&(x))[_D0] & _DSIGN)
 #define HUGE_EXP    (int)(_DMAX * 900L / 1000)
-#define HUGE_RAD    3.37e9
+#define HUGE_RAD    3.37e30
 #define SAFE_EXP    (_DMAX>>1)
 
 // word offsets within double
