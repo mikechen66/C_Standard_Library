59c6655c59952b185822cdfa59d4344a48e2babf
mikechen66 <875661606@qq.com>
2023-12-05 11:01:05 +0800

    Add files via upload

--- Plauger_C_Library/STDDEF/tstddef.c
+++ Plauger_C_Library/STDDEF/tstddef.c
@@ -16,6 +16,7 @@ typedef struct {
 
 // static data
 static char *pc = NULL;
+static double *pd = NULL;
 static size_t offs[] = {
     offsetof(Str, f1),
     offsetof(Str, f2),
@@ -24,21 +25,22 @@ static size_t offs[] = {
 int main() {    
 
     // test basic workings of stddef definitions
-    ptrdiff_t pdiff = &pc[INT_MAX] - &pc[0];
+    ptrdiff_t pd = &pc[INT_MAX] - &pc[0];
     wchar_t wc = L'Z';
     Str x = {1, 2, 3};
     char *ps = (char *)&x;
 
-    assert(sizeof (size_t) == sizeof (sizeof (char)));
-    assert(pdiff == &pc[INT_MAX] - &pc[0]);
+    assert(sizeof(ptrdiff_t) == sizeof(size_t));
+    assert(sizeof(size_t) == sizeof(sizeof(char)));
+    assert(pd == &pc[INT_MAX] - &pc[0]);
     assert(wc == L'Z');
     assert(offs[0] < offs[1]);
     assert(offs[1] < offs[2]);
     assert(*(char *)(ps + offs[0]) == 1);
     assert(*(float *)(ps + offs[1]) == 2);
     assert(*(int *)(ps + offs[2]) == 3);
-    printf("sizeof (size_t) = %u\n", sizeof (size_t));
-    printf("sizeof (wchar_t) = %u\n", sizeof (wchar_t));
+    printf("sizeof(size_t) = %u\n", sizeof(size_t));
+    printf("sizeof(wchar_t) = %u\n", sizeof(wchar_t));
     puts("SUCCESS testing <stddef.h>");
     return (0);
 }
