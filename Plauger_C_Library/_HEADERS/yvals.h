ffeb952dc8fd419e18d74bebea0e6a061e3c0b09
mikechen66 <875661606@qq.com>
2023-12-09 00:17:14 +0800

    Add files via upload

--- Plauger_C_Library/_HEADERS/yvals.h
+++ Plauger_C_Library/_HEADERS/yvals.h
@@ -1,11 +1,12 @@
 /* yvals.h values header -- Turbo C++ version */
+
 #define _YVALS
-		/* errno properties */
+// errno properties
 #define _EDOM	33
 #define _ERANGE	34
 #define _EFPOS	35
 #define _ERRMAX	36
-		/* float properties */
+// float properties
 #define _D0		3
 #define _DBIAS	0x3fe
 #define _DLONG	1
@@ -15,17 +16,17 @@
 #define _FRND	1
 #define _LBIAS	0x3ffe
 #define _LOFF	15
-		/* integer properties */
+// integer properties
 #define _C2		1
 #define _CSIGN	1
 #define _ILONG	0
 #define _MBMAX	8
 typedef unsigned short _Wchart;
-		/* pointer properties */
+// pointer properties
 #define _NULL	(void *)0
 typedef int _Ptrdifft;
 typedef unsigned int _Sizet;
-		/* setjmp properties */
+// setjmp properties
 #define _NSETJMP	10
 #define _Setjmp(env)	setjmp(env)
 #ifdef __cplusplus
@@ -33,20 +34,19 @@ extern "C" int setjmp(int *);
 #else
 int setjmp(int *);
 #endif
-		/* signal properties */
+// signal properties
 #define _SIGABRT	22
 #define _SIGMAX		32
-		/* stdio properties */
+// stdio properties
 #define _FNAMAX	64
 #define _FOPMAX	16
 #define _TNAMAX	16
-		/* stdlib properties */
+// stdlib properties
 #define _EXFAIL	1
-		/* storage alignment properties */
+// storage alignment properties
 #define _AUPBND	1U
 #define _ADNBND	1U
 #define _MEMBND	1U
-		/* time properties */
+// time properties
 #define _CPS	1
 #define _TBIAS	((70 * 365LU + 17) * 86400)
-
